54b53c37321435ce473a38ab7f70b9c982bf07ec
Nana <nanagalaxy385@gmail.com>
2025-05-05 18:11:54 +0100

    json and csv specific handler function logic implemented

--- gdpr_obfuscator/csv_handler.py
+++ gdpr_obfuscator/csv_handler.py
@@ -1,3 +1,35 @@
-def csv_handler(bucket, file_name):
-    print(f"CSV Handler called for file: {file_name}")
+import boto3
+import pandas as pd
+from io import StringIO
+
+s3 = boto3.client('s3')
+
+
+def csv_processor(bucket, file_name, pii_fields):
+    print(f"CSV Handler called for file: {file_name} in bucket: {bucket}")
+    
+    obj = s3.get_object(Bucket=bucket, Key=file_name)
+    csv_data = obj['Body'].read().decode('utf-8')
+    df = pd.read_csv(StringIO(csv_data))
+    
+    for field in pii_fields:
+        if field in df.columns:
+            df[field] = '*' * df[field].astype(str).str.len()
+            
+    obfuscated_csv = df.to_csv(index=False)
+    obfuscated_file_name = f"obfuscated_{file_name}"
+    s3.put_object(Bucket=bucket, Key=obfuscated_file_name, Body=obfuscated_csv)
+    
     return {'statusCode': 200, 'body': 'CSV routed successfully'}
+
+def lambda_handler(event, context):
+    s3_uri = event.get('file_to_obfuscate')
+    pii_fields = event.get('pii_fields', [])
+    
+    no_prefix = s3.uri.replace('s3://', '')
+    parts = no_prefix.split('/', 1)
+    bucket = parts[0]
+    file_name = parts[1] if len(parts) > 1 else ''
+    
+    
+    return csv_processor(bucket, file_name, pii_fields)
--- gdpr_obfuscator/json_handler.py
+++ gdpr_obfuscator/json_handler.py
@@ -1,4 +1,38 @@
-def json_handler(bucket, file_name):
-    print(f"Json Handler called for file: {file_name}")
-    return {'statusCode': 200, 'body': 'Json file routed successfully'}
+import boto3
+import json
+import pandas as pd
+import io
+
+s3_client = boto3.client('s3')
+
+
+
+def json_processor(bucket, file_name, pii_fields):
+    print(f"Json Handler called for file: {file_name} in bucket: {bucket}")
+    
+    response = s3_client.get_object(Bucket=bucket, Key=file_name)
+    content = response['Body'].read().decode('utf-8')
+    records = json.loads(content)
+    
+    df =pd.DataFrame(records)
+    
+    for field in pii_fields:
+        if field in df.columns:
+            df[field] = df[field].apply(lambda x: '*' * len(str(x)) if pd.notna(x) else x)
+
+    obfuscated_data = df.to_dict(orient='records')
+    obfuscated_json = json.dumps(obfuscated_data, indent=2).encode('utf-8')
+
+    output_key = f"obfuscated/{file_name.split('/')[-1]}"
+    s3_client.put_object(Bucket=bucket, Key=output_key, Body=obfuscated_json)
+
+    return {'statusCode': 200, 'body': f'JSON file obfuscated and saved to {output_key}'}
+            
+
+
+def lambda_handler(event, context):
+    bucket = event.get('bucket')
+    file_name = event.get('file_name')
+    pii_fields = event.get('pii_fields', [])
+    return json_processor(bucket, file_name, pii_fields)
     
